724fbf6e179a46f044980d48845447926fde1244
AlmondDust <adam.s.buchinsky@gmail.com>
2021-12-07 22:41:59 -0500

    run single trial in figure 17

--- aifo_simulation/java-code/src/main/java/ch/ethz/systems/netbench/core/run/MainFigure17.java
+++ aifo_simulation/java-code/src/main/java/ch/ethz/systems/netbench/core/run/MainFigure17.java
@@ -3,5 +3,7 @@ package ch.ethz.systems.netbench.core.run;
 public class MainFigure17 {
     public static void main(String args[]) {
         System.out.println("Creating Figure 17");
+        MainFromProperties.main(new String[]{"projects/aifo/runs/aifo_evaluation/pFabric/web_search_workload_q_len_and_K/8900/K1/AIFO_C100.properties"
+                , "second_transport_layer=udp", "traffic_lambda_flow_starts_per_s=89000", "window_size=20", "output_port_max_size_packets=100", "link_bandwidth_bit_per_ns=10", "sample_count=15", "k_value=0.1"});
     }
 }
