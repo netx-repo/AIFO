be0bef3ba1be1a4074e52a406e94ac15d3831c0d
AlmondDust <adam.s.buchinsky@gmail.com>
2021-12-09 18:52:30 -0500

    stuff to help deal with exceptions

--- aifo_simulation/java-code/src/main/java/ch/ethz/systems/netbench/core/run/MainFigure17.java
+++ aifo_simulation/java-code/src/main/java/ch/ethz/systems/netbench/core/run/MainFigure17.java
@@ -8,18 +8,45 @@ public class MainFigure17 {
         double k_values[] = {0.1, 0.3, 0.7, 0.9};
         int q_lens[] = {100, 250, 375, 500};
 
-        for (int load : loads) {
-            for (double k_value : k_values) {
-                for (int q_len : q_lens) {
-                    String folderName = String.format("AIFO_L%d_C%d_K%d", load, q_len, (long)(k_value*100));
+        int start_trial = 12; //first trial is 1
 
-                    String arg1 = String.format("traffic_lambda_flow_starts_per_s=%d", load);
-                    String arg2 = String.format("k_value=%,.2f", k_value);
-                    String arg3 = String.format("output_port_max_size_packets=%d", q_len);
+
+        int trial_count= 0;
+        for (int i = 0; i < loads.length; i++) {
+            for (int j = 0; j < k_values.length; j++) {
+                for (int k = 0; k < q_lens.length; k++) {
+                    String folderName = String.format("AIFO_L%d_C%d_K%d", loads[i], q_lens[k], (long)(k_values[j]*100));
+
+                    String arg1 = String.format("traffic_lambda_flow_starts_per_s=%d", loads[i]);
+                    String arg2 = String.format("k_value=%,.2f", k_values[j]);
+                    String arg3 = String.format("output_port_max_size_packets=%d", q_lens[k]);
                     String arg4 = String.format("run_folder_name=%s", folderName);
 
-                    MainFromProperties.main(new String[]{"projects/aifo/runs/aifo_evaluation/pFabric/web_search_workload_q_len_and_K/AIFO_DEFAULT.properties"
-                , "second_transport_layer=udp", arg1, "window_size=20", arg3, "link_bandwidth_bit_per_ns=10", "sample_count=15", arg2, arg4});
+                    trial_count++;
+                    if(trial_count < start_trial){
+                        System.out.println(String.format("Skipping trial #%d: %s", trial_count, folderName));
+                    } else {
+                        System.out.println(String.format("Running trial #%d: %s", trial_count, folderName));
+
+                        boolean trial_succeeded = false;
+                        int trial_attempts_remaining = 5;
+
+                        while(!trial_succeeded){
+                            try {
+                                trial_attempts_remaining--;
+                                MainFromProperties.main(new String[]{"projects/aifo/runs/aifo_evaluation/pFabric/web_search_workload_q_len_and_K/AIFO_DEFAULT.properties", "second_transport_layer=udp", arg1, "window_size=20", arg3, "link_bandwidth_bit_per_ns=10", "sample_count=15", arg2, arg4});
+                                trial_succeeded = true;
+                            } catch (Exception e) {
+                                System.out.println(e);
+                                if(trial_attempts_remaining >= 0){
+                                    System.out.println(String.format("Trying again\nTrial attempts remaining: %d", trial_attempts_remaining));
+                                } else {
+                                    System.out.println("Failed too much, exiting...");
+                                    System.exit(1);
+                                }
+                            }
+                        }
+                    }
 
                 }
             }
